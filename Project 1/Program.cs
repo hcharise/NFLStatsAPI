ff089575c0e6658922e37001691bbf6849d29b80
hcharise <110205350+hcharise@users.noreply.github.com>
2025-01-25 16:12:57 -0700

    Add project files.

---
+++ Project 1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            Item item = await JsonFileReader.ReadAsync<Item>(@"C:\Users\hchar\OneDrive\Other\Desktop\team49ers_season2020_a.json");
+        }
+    }
+
+    public static class JsonFileReader
+    {
+        public static async Task<T> ReadAsync<T>(string filePath)
+        {
+            using FileStream stream = File.OpenRead(filePath);
+            return await JsonSerializer.DeserializeAsync<T>(stream);
+        }
+    }
+
+    public class Item
+    {
+        public int millis;
+        public string stamp;
+        public DateTime datetime;
+        public string light;
+        public float temp;
+        public float vcc;
+    }
+}
