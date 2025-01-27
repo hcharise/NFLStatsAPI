48f0d4caf819bcfa183a15ffd03904d212d7b107
hcharise <110205350+hcharise@users.noreply.github.com>
2025-01-26 21:42:41 -0700

    Added to to dos

--- Project 1/Program.cs
+++ Project 1/Program.cs
@@ -17,6 +17,7 @@ using Newtonsoft.Json;
 // TO DOS:
 //  - Rename Root and put all json handling into json package? - may already be done?
 //  - Clean up code - methods to print menu, handle menu options?, print intro message, tasks in each of the switch statements
+//  - Any other error/unexpected input that might need to be handled? give option to re-enter URL?
 //  - Add comments - adequately detailed module operations and maintenance history as well as function prologue, annotated throughout
 //  - Check that methods are less than 50 lines each
 //  - Create documentation - model of classes and relationships
