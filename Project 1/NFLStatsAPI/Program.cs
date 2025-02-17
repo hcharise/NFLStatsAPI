50142d76b71f29b8bffe9898bc0236299a5694bb
hcharise <110205350+hcharise@users.noreply.github.com>
2025-02-17 09:41:43 -0700

    Updated naming to reflect multiphase project; created to do list for phase 2

--- Project 1/NFLStatsAPI/Program.cs
+++ Project 1/NFLStatsAPI/Program.cs
@@ -2,6 +2,33 @@
 /// Entry point for the NFL Game Stats Processor application.
 /// This class initializes dependencies, loads game data, and starts the menu handler.
 /// </summary>
+///  
+/// PHASE 2
+/// 
+/// 
+/// PHASE 1
+/// Program takes URL from user, accesses data, then can print data for a single game, all games, or exit the program.
+/// 
+/// 
+/// TO DO:
+///  - Modify URL taker so that it can pull all 32 games
+///         (for (1-32) {
+///             get URL[i] (concat?)
+///             then print team's name & number
+///             for (all games in their season) {
+///                 if win count as win; if loss count as loss; if tie count as tie
+///             }
+///             print out record
+///         }
+///  - Add a message queue implementation to regulate the requests going to your server
+///  - Modify output to print out teams’ names, associated team number, and their season records
+///  - Add for loop to count team's records
+///  - Add notes above about phase 2 changes
+///  - Update/redo documentation - add a diagram that shows context? try to use C4 model?, where is message queue & how does it work
+///  - Figure out why teams are numbered the way they are?
+///  
+
+
 
 using System;
 using System.Threading.Tasks;
